5bd08433880fc93877572f0a99197eee4b8c97d2
MajinBuu62 <checchia2001@gmail.com>
2026-07-14 12:22:11 -0400

    Update core_timing.cpp

--- src/core/core_timing.cpp
+++ src/core/core_timing.cpp
@@ -200,7 +200,7 @@ u64 CoreTiming::GetClockTicks() const {
     const auto overclock = Settings::values.fast_cpu_time.GetValue();
 
      if (overclock != Settings::CpuClock::Off) {
-         fres = (u64) ((double) fres * (1.7 + 0.3 * u32(overclock)));
+         fres = (u64) ((double) fres * (1.7 + 2.0 * u32(overclock)));
      }
 
      if (Settings::values.sync_core_speed.GetValue()) {
